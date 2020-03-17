99062b04790cb84cf55b1d59cd946c09590449e2
br prod <brprod@mail.com>
2020-03-17 12:13:15 -0300

    precio desde propiedad

--- js/propiedad.js
+++ js/propiedad.js
@@ -1,2 +1,7 @@
 const urlParams = new URLSearchParams(window.location.search)
 const id = urlParams.get('id')
+const propiedad = propiedades.find( propiedad => propiedad.id == id )
+
+// Precio
+const priceEl = document.querySelector('.price')
+priceEl.append(propiedad.valor)
