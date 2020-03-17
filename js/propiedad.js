0a6b3f86c067f81623c754d3c0b51b7c3a4f12b7
br prod <brprod@mail.com>
2020-03-17 12:19:08 -0300

    direccion

--- js/propiedad.js
+++ js/propiedad.js
@@ -5,3 +5,7 @@ const propiedad = propiedades.find( propiedad => propiedad.id == id )
 // Precio
 const priceEl = document.querySelector('.price')
 priceEl.append(propiedad.valor)
+
+// Direccion
+const direccionEl = document.querySelector('.title-area h2')
+direccionEl.append(propiedad.direccion)
