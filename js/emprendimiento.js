abdf33e339d262e033792d1626c9952d87f1d7de
brprddrprb <brprodprodbr@gmail.com>
2020-06-08 20:40:07 -0300

    agregando boton whatsapp

--- js/emprendimiento.js
+++ js/emprendimiento.js
@@ -1,109 +1,109 @@
-const urlParams = new URLSearchParams(window.location.search)
-const id = urlParams.get('id')
+const urlParams = new URLSearchParams(window.location.search);
+const id = urlParams.get('id');
 
 
 
 
 
 const checkEmprendimientos = async () => {
-  let emprendimientoData = await fetch('../emprendimientos.json')
-  let emprendimientoJSON = await emprendimientoData.json()
+  let emprendimientoData = await fetch('../emprendimientos.json');
+  let emprendimientoJSON = await emprendimientoData.json();
   return emprendimientoJSON[id - 1]
-}
+};
 
 const getEmprendimientos = async () => {
-  const emprendimiento = await checkEmprendimientos()
+  const emprendimiento = await checkEmprendimientos();
   setupHTML(emprendimiento)
 
-}
+};
 
-getEmprendimientos()
+getEmprendimientos();
 
 function setupHTML(emprendimiento) {
 
   // Set title
-  document.querySelector('.titleArea h2').innerHTML += emprendimiento.nombre
+  document.querySelector('.titleArea h2').innerHTML += emprendimiento.nombre;
 
   // Set address
-  document.querySelector('.titleArea h4').innerHTML += emprendimiento.localidad + ', ' + emprendimiento.partido + '<br>' + emprendimiento.direccion
+  document.querySelector('.titleArea h4').innerHTML += emprendimiento.localidad + ', ' + emprendimiento.partido + '<br>' + emprendimiento.direccion;
 
   // Set type
-  document.querySelector('.type > div:nth-child(1)').innerHTML += emprendimiento.tipo
+  document.querySelector('.type > div:nth-child(1)').innerHTML += emprendimiento.tipo;
 
   // Set state
-  document.querySelector('.prices > div:nth-child(1)').innerHTML += emprendimiento.estado
+  document.querySelector('.prices > div:nth-child(1)').innerHTML += emprendimiento.estado;
 
   // Set Description
-  document.querySelector('.des').innerHTML += emprendimiento.descripcion
+  document.querySelector('.des').innerHTML += emprendimiento.descripcion;
 
-  addTags(emprendimiento.tags)
+  addTags(emprendimiento.tags);
 
   // Set atras
   document.querySelector('div.buttonsContent:nth-child(2) > button:nth-child(1)')
-    .addEventListener('click', function () { document.location.href = 'emprendimientos.html' })
+    .addEventListener('click', function () { document.location.href = 'emprendimientos.html' });
 
   // Set email
   document.querySelector('div.buttonsContent:nth-child(2) > button:nth-child(2)')
-    .addEventListener('click', function () { document.location.href = 'mailto: ariel.sauret@gmail.com' })
+    .addEventListener('click', function () { document.location.href = 'mailto: ariel.sauret@gmail.com' });
 
   // Set whatsapp
   document.querySelector('button.btnWhatsapp:nth-child(4)').addEventListener('click',
-    function () { document.location.href = 'https://wa.me/541158430937' })
+    function () { document.location.href = 'https://wa.me/541158430937' });
 
   // Set call btn
   document.querySelector('button.btnCall:nth-child(3)').addEventListener('click',
-    function () { document.location.href = 'tel:+541158430937' })
+    function () { document.location.href = 'tel:+541158430937' });
 
   const btnGalleryNext = document.querySelector('button.control:nth-child(4)');
   const btnGalleryPrev = document.querySelector('button.control:nth-child(3)');
   // Add img to gallery
-  addGalleryImg(emprendimiento.galeria, document.querySelector('.galleryContent'))
-  const elements = Array.from(document.getElementsByName('gallery'))
-  btnGalleryNext.addEventListener('click', goToNext)
-  btnGalleryPrev.addEventListener('click', goToPrev)
+  addGalleryImg(emprendimiento.galeria, document.querySelector('.galleryContent'));
+  const elements = Array.from(document.getElementsByName('gallery'));
+  btnGalleryNext.addEventListener('click', goToNext);
+  btnGalleryPrev.addEventListener('click', goToPrev);
 
 
   function goToPrev() {
-    const max = elements.length - 1
+    const max = elements.length - 1;
 
-    let currentPos = elements.findIndex(x => x.classList.contains('active'))
-    if (currentPos == -1) currentPos = 0
-    const previousPos = (currentPos == 0) ? max : currentPos - 1
-    const followingPos = (currentPos == max) ? 0 : currentPos + 1
+    let currentPos = elements.findIndex(x => x.classList.contains('active'));
+    if (currentPos === -1) currentPos = 0;
+    const previousPos = (currentPos === 0) ? max : currentPos - 1;
+    const followingPos = (currentPos === max) ? 0 : currentPos + 1;
 
     const previous = elements[previousPos],
       current = elements[currentPos],
-      following = elements[followingPos]
+      following = elements[followingPos];
 
     // previous
-    previous.classList.remove('prepared', 'before', 'active')
+    previous.classList.remove('prepared', 'before', 'active');
     // current
-    current.classList.add('before')
-    current.classList.remove('prepared', 'active')
+    current.classList.add('before');
+    current.classList.remove('prepared', 'active');
     // folowing
-    following.classList.add('active')
+    following.classList.add('active');
     following.classList.remove('prepared', 'after')
   }
 
   function goToNext() {
-    const max = elements.length - 1
+    const max = elements.length - 1;
 
-    let currentPos = elements.findIndex(x => x.classList.contains('active'))
-    if (currentPos == -1) currentPos = 0
-    const previousPos = (currentPos == 0) ? max : currentPos - 1
-    const followingPos = (currentPos == max) ? 0 : currentPos + 1
+    let currentPos = elements.findIndex(x => x.classList.contains('active'));
+    if (currentPos === -1) currentPos = 0;
+    const previousPos = (currentPos === 0) ? max : currentPos - 1;
+    const followingPos = (currentPos === max) ? 0 : currentPos + 1;
 
     const previous = elements[previousPos],
       current = elements[currentPos],
-      following = elements[followingPos]
+      following = elements[followingPos];
 
     // folowing
-    following.classList.remove('prepared', 'after', 'active')
+    following.classList.remove('prepared', 'after', 'active');
     // current
-    current.classList.add('after')
-    current.classList.remove('prepared', 'active')
+    current.classList.add('after');
+    current.classList.remove('prepared', 'active');
     // previous
-    previous.classList.add('active')
+    previous.classList.add('active');
     previous.classList.remove('prepared', 'before')
   }
 
@@ -111,7 +111,7 @@ function setupHTML(emprendimiento) {
 
 
 function addGalleryImg(imgs, galleryContent) {
-  let markup = ''
+  let markup = '';
   imgs.forEach(img => {
     markup += `
     <div data-v-583f8fa1="" name="gallery" class="item"><div data-v-0d283d01="" data-v-53ef937b="" class="image imageArea adaptative scoped" data-v-583f8fa1=""><img data-v-0d283d01="" src="${img}" alt="imagen de propiedad"></div></div>
@@ -120,24 +120,24 @@ function addGalleryImg(imgs, galleryContent) {
   galleryContent.innerHTML = markup
 }
 
-const detalle = document.querySelector('.des')
+const detalle = document.querySelector('.des');
 document.querySelector('button.input')
-  .addEventListener('click', toggleDescription)
+  .addEventListener('click', toggleDescription);
 
 function toggleDescription() {
-  detalle.classList.toggle('close')
+  detalle.classList.toggle('close');
   detalle.classList.toggle('open')
 }
 
 function addTags(tags) {
   function getDisplayFlex(tagData) {
-    return (tagData != '' ? 'flex' : 'none')
+    return (tagData !== '' ? 'flex' : 'none')
   }
   function getDisplayBlock(tagData) {
-    return (tagData != '' ? 'block' : 'none')
+    return (tagData !== '' ? 'block' : 'none')
   }
-  const tagsDev = document.querySelector('ul.tags.dev')
-  let markup = ''
+  const tagsDev = document.querySelector('ul.tags.dev');
+  let markup = '';
   tags.forEach(tag => {
     markup = `
       <li data-v-7f9ee979="">
@@ -149,7 +149,7 @@ function addTags(tags) {
       <span data-v-2c0f8a5b="" class="valueTag" style="display: ${getDisplayBlock(tag.valueTag)};">${tag.valueTag}</span>
       <span data-v-2c0f8a5b="" class="value"><small data-v-2c0f8a5b="">${tag.value}</small></span>
       <span data-v-2c0f8a5b="" class="description">${tag.description}</span></div>
-      </li>`
+      </li>`;
     tagsDev.innerHTML += markup
   });
 }
