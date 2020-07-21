c9c43cc743417911e1000c8809a4b72838ac802d
brprddrprb <brprodprodbr@gmail.com>
2020-07-20 22:01:59 -0300

    start

--- js/index.js
+++ js/index.js
@@ -1,14 +1,25 @@
 // Random img del background home
-function randomImg() {
-  const img = ['img/home-a-mob@2x.jpg',
-               'img/home-b-mob@2x.jpg',
-               'img/home-c-mob@2x.jpg']
-
-  document.querySelector('.home-area')
-   .style.background = `url(${img[Math.floor(Math.random() * 3)]}) repeat-x transparent`
+const bg = document.querySelector('.home-area')
+const imgMobile = [
+              'img/home-a-mob@2x.jpg',
+              'img/home-b-mob@2x.jpg',
+              'img/home-c-mob@2x.jpg'
+            ];
+const imgWide = [
+                'https://source.unsplash.com/random/1920x1080',
+                'https://source.unsplash.com/random/1921x1080',
+                'https://source.unsplash.com/random/1922x1080'
+                ];
+function getImgArray() {
+  return screen.width > 640 ? imgWide : imgMobile
 }
 
-randomImg() 
+function randomImg() {
+  bg.style.background = `url(${getImgArray()[Math.floor(Math.random() * 3)]}) repeat-x transparent`
+}
+
+randomImg();
+window.addEventListener('resize', randomImg)
 
 // Estado inicial
 // transition: all 0.15s ease-in-out 0s; transform: translate(0px);
